cd40be6e7fba0cda0f21da51b7d59dc5c9808464
Joseph Bowers <jsphbowers@gmail.com>
2023-04-05 11:12:16 -0600

    Tower Events Server Done

--- TowerEvents/server/controllers/TowerEventsController.js
+++ TowerEvents/server/controllers/TowerEventsController.js
@@ -1,14 +1,29 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 import { towerEventsService } from "../services/TowerEventsService.js";
+import { dbContext } from "../db/DbContext.js";
 
 export class TowerEventsController extends BaseController {
   constructor() {
     super('api/events')
     this.router
-
+      .get('', this.getAllEvents)
+      .get('/:id', this.getTowerEvent)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
+      .put('/:id', this.editEvent)
+      .delete('/:id', this.archive)
+  }
+
+  async editEvent(req, res, next) {
+    try {
+      let towerEventEdits = req.body
+      let towerEventId = req.params.id
+      let editedTowerEvent = await towerEventsService.editEvent(towerEventEdits, towerEventId)
+      return res.send(editedTowerEvent)
+    } catch (error) {
+      next(error);
+    }
   }
 
   async create(req, res, next) {
@@ -21,9 +36,31 @@ export class TowerEventsController extends BaseController {
       next(error);
     }
   }
-  async getAll(req, res, next) {
+  async getAllEvents(req, res, next) {
     try {
+      let towerEvents = await towerEventsService.getAllEvents()
+      return res.send(towerEvents)
+    } catch (error) {
+      next(error);
+    }
+  }
 
+  async getTowerEvent(req, res, next) {
+    try {
+      let towerEventId = req.params.id
+      let towerEvent = await towerEventsService.getTowerEvent(towerEventId)
+      return res.send(towerEvent)
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async archive(req, res, next) {
+    try {
+      let userId = req.userInfo.id
+      let towerEventId = req.params.id
+      let message = await towerEventsService.archive(userId, towerEventId)
+      return res.send(message)
     } catch (error) {
       next(error);
     }
--- TowerEvents/server/services/TowerEventsService.js
+++ TowerEvents/server/services/TowerEventsService.js
@@ -1,12 +1,70 @@
 import { dbContext } from "../db/DbContext.js";
+import { BadRequest, Forbidden, UnAuthorized } from "../utils/Errors.js";
 
 class TowerEventsService {
+  // GET ONE EVENT OR MANY
+  async getTowerEvent(towerEventId) {
+    let towerEvent = await dbContext.TowerEvents.findById(towerEventId)
+    if (towerEvent == null) {
+      throw new BadRequest('There is no corresponding event, please check your data and try again.')
+    }
+    return towerEvent
+  }
+  async getAllEvents() {
+    let towerEvents = await dbContext.TowerEvents.find()
+      .populate('creator', 'picture name')
+    return towerEvents
+  }
+
+
+  // CREATING || EDITING || Archiving
   async createTowerEvent(towerEventData) {
     const newTowerEvent = await dbContext.TowerEvents.create(towerEventData)
     await newTowerEvent.populate("creator", 'picture name')
     return newTowerEvent
   }
 
+  async editEvent(towerEventEdits, towerEventId) {
+    let originalTowerEvent = await this.getTowerEvent(towerEventId)
+
+    if (!originalTowerEvent) {
+      throw new BadRequest('There is no tower with the provided credentials. Try again.')
+    }
+
+    if (originalTowerEvent.isCanceled == true) {
+      throw new BadRequest('This event has already been canceled.')
+    }
+
+    originalTowerEvent.name = towerEventEdits.name ? towerEventEdits.name : originalTowerEvent.name
+    originalTowerEvent.description = towerEventEdits.description ? towerEventEdits.description : originalTowerEvent.description
+    originalTowerEvent.coverImg = towerEventEdits.coverImg ? towerEventEdits.coverImg : originalTowerEvent.coverImg
+    originalTowerEvent.location = towerEventEdits.location ? towerEventEdits.location : originalTowerEvent.location
+    originalTowerEvent.capacity = towerEventEdits.capacity ? towerEventEdits.capacity : originalTowerEvent.capacity
+    originalTowerEvent.startDate = towerEventEdits.startDate ? towerEventEdits.startDate : originalTowerEvent.startDate
+    originalTowerEvent.type = towerEventEdits.type ? towerEventEdits.type : originalTowerEvent.type
+
+
+    await originalTowerEvent.save()
+
+    return originalTowerEvent
+  }
+
+  async archive(userId, towerEventId) {
+    let originalTowerEvent = await (await this.getTowerEvent(towerEventId)).populate('creator')
+    if (!originalTowerEvent) {
+      throw new BadRequest('There is no tower with the provided credentials. Try again.')
+    }
+    if (originalTowerEvent.creatorId != userId) {
+      throw new UnAuthorized(`You are not the creator of ${originalTowerEvent.name}, be gone long one!`)
+    }
+    if (originalTowerEvent.isCanceled == true) {
+      throw new Forbidden(`${originalTowerEvent.name} has already been canceled.`)
+    }
+
+    originalTowerEvent.isCanceled = true
+    await originalTowerEvent.save()
+    return `${originalTowerEvent.name} was successfully canceled!`
+  }
 }
 
 export const towerEventsService = new TowerEventsService();
